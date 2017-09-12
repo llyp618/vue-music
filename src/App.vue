81f6c295c7cb7e133d0d9eb8fbe372f28698238c
llyp618 <llyp618@163.com>
2017-09-12 23:52:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        hello world
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+    @import "./common/less/variable";
+    #app {
+        color:@color-theme;
+    }
+</style>
